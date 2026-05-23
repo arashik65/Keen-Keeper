077bd47a8e058667a8a08f9fb049b7558b685684
arashik65 <mdashikur6398+@gmail.com>
2026-05-23 22:41:21 +0600

    layout section designed

--- src/app/Providers/index.js
+++ src/app/Providers/index.js
@@ -1,1 +1,14 @@
 
+"use client";
+import PathNameProvider from "@/context/pathNameContext";
+import TimeLineProvider from "@/context/timeLineContext";
+import { Children } from "react";
+
+const Providers = ({ children }) => {
+  return (
+    <TimeLineProvider>
+      <PathNameProvider>{children}</PathNameProvider>
+    </TimeLineProvider>
+  );
+};
+export default Providers;
--- src/app/friend/[id]/layout.jsx
+++ src/app/friend/[id]/layout.jsx
@@ -1,1 +1,8 @@
 
+import React from "react";
+
+const FriendDetailsLayout = ({ children }) => {
+  return <div className=" bg-gray-100 text-white">{children}</div>;
+};
+
+export default FriendDetailsLayout;
