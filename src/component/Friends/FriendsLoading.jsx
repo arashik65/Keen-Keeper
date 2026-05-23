a88c53cedb5c45060f3b5b839a5bddfe3cc9e754
arashik65 <mdashikur6398+@gmail.com>
2026-05-23 22:24:46 +0600

    friend section designed

--- src/component/Friends/FriendsLoading.jsx
+++ src/component/Friends/FriendsLoading.jsx
@@ -1,1 +1,12 @@
 
+import React from "react";
+
+const FriendsLoading = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen  ">
+      <span className="loading loading-spinner loading-xl scale-150"></span>
+    </div>
+  );
+};
+
+export default FriendsLoading;
