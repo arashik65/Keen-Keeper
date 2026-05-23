959dbadd9bc7edef66bedd65654ba6944aae2bf1
arashik65 <mdashikur6398+@gmail.com>
2026-05-23 22:34:44 +0600

    timeline section designed

--- src/app/timeline/layout.jsx
+++ src/app/timeline/layout.jsx
@@ -1,1 +1,12 @@
 
+import React from "react";
+
+export const metadata = {
+  title: "Timeline - Keen Keeper",
+  description: "This is the timeline page of Keen Keeper.",
+};
+const TimeLineLayout = ({ children }) => {
+  return <div className=" bg-gray-100">{children}</div>;
+};
+
+export default TimeLineLayout;
